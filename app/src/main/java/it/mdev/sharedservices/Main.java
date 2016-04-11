77168c481923cbcaec62243e1e5cbeefab087ebe
Salem <salemmosbahi@gmail.com>
2016-04-11 13:56:39 +0100

    first commit

---
+++ app/src/main/java/it/mdev/sharedservices/Main.java
@@ -0,0 +1,164 @@
+package it.mdev.sharedservices;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import it.mdev.sharedservices.activity.Car;
+import it.mdev.sharedservices.activity.Download;
+import it.mdev.sharedservices.activity.Event;
+import it.mdev.sharedservices.activity.Home;
+import it.mdev.sharedservices.activity.Login;
+import it.mdev.sharedservices.activity.Paper;
+import it.mdev.sharedservices.activity.Profile;
+import it.mdev.sharedservices.activity.Settings;
+import it.mdev.sharedservices.design.FragmentDrawer;
+import it.mdev.sharedservices.util.Controllers;
+
+import it.mdev.sharedservices.design.FragmentDrawer;
+
+public class Main extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener{
+    private SharedPreferences pref;
+    Controllers conf = new Controllers();
+
+    public Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        pref = getSharedPreferences(conf.app, Context.MODE_PRIVATE);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        drawerFragment = (FragmentDrawer) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
+        displayView(0);
+        if(!pref.getString(conf.tag_token, "").equals("")){
+            RelativeLayout rl = (RelativeLayout) findViewById(R.id.nav_header_container);
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View vi = inflater.inflate(R.layout.toolnav_drawer, null);
+            TextView tv = (TextView) vi.findViewById(R.id.usernameTool_txt);
+            tv.setText(pref.getString(conf.tag_username, ""));
+            ImageView im = (ImageView) vi.findViewById(R.id.pictureTool_iv);
+            byte[] imageAsBytes = Base64.decode(pref.getString(conf.tag_picture, "").getBytes(), Base64.DEFAULT);
+            im.setImageBitmap(BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length));
+            rl.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.container_body, new Profile());
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                    getSupportActionBar().setTitle(getString(R.string.profile));
+
+                }
+            });
+            rl.addView(vi);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        /*if(id == R.id.action_notify){
+            Toast.makeText(getApplicationContext(), "Search action is selected!", Toast.LENGTH_SHORT).show();
+            return true;
+        }*/
+        if (id == R.id.action_settings) {
+            displayView(5);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
+
+    public void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new Home();
+                title = getString(R.string.app_name);
+                break;
+            case 1:
+                fragment = new Car();
+                title = getString(R.string.car);
+                break;
+            case 2:
+                fragment = new Download();
+                title = getString(R.string.download);
+                break;
+            case 3:
+                fragment = new Event();
+                title = getString(R.string.event);
+                break;
+            case 4:
+                fragment = new Paper();
+                title = getString(R.string.paper);
+                break;
+            case 5:
+                fragment = new Settings();
+                title = getString(R.string.settings);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            if( pref.getString(conf.tag_token, "").equals("")) {
+                if (title.equals(getString(R.string.app_name)) || title.equals(getString(R.string.about))) {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.container_body, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                    getSupportActionBar().setTitle(title);
+                } else {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.container_body, new Login());
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                    getSupportActionBar().setTitle(getString(R.string.login));
+                }
+            } else {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_body, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                getSupportActionBar().setTitle(title);
+            }
+        }
+    }
+}
